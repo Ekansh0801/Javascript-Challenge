b678530bc901874d654dde733fd942477cbbdfcb
Ekansh0801 <ekanshaggarwal11103@gmail.com>
2024-07-13 20:57:27 +0530

    Day-01

---
+++ Day-01/script.js
@@ -0,0 +1,40 @@
+//Activity 1 Variable declaration
+var a = 1;
+console.log(a);
+
+let b = "Hello World";
+console.log(b)
+
+//Activity 2 Constant Declaration
+const c = true;
+console.log(c)
+
+//Activity 3 Data types 
+let num=88;
+let str="Javascript";
+let bool=true;
+let arr = ["Hello","World","Programming"];
+let obj={
+    name:"Ekansh",
+    age:20
+}
+
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof arr);
+console.log(typeof obj);
+
+//Activity 4 Reassigning values
+let x = 1
+console.log("Initial -> ",x);
+
+y = 2;
+console.log("Re-assigned -> ",y);
+
+//Activity 5 Understanding Const
+const val=true;
+console.log("const inital value ->",val);
+
+val=false;
+console.log("const re-assigned value -> ",val);
