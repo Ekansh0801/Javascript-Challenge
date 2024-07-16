f3d30fc6c5e6260e0fcb6b9bcceca3be170e1005
Ekansh0801 <ekanshaggarwal11103@gmail.com>
2024-07-16 23:36:00 +0530

    Day-02

---
+++ Day-02/script.js
@@ -0,0 +1,47 @@
+//Activity 1
+let a = 4,b = 5
+let sum = a + b
+
+console.log(sum);
+
+let sub = b - a
+console.log(sub);
+
+let mul = a * b
+console.log(mul);
+
+let div = b / a
+console.log(div);
+
+
+let rem = b % a
+console.log(rem);
+
+//Activity 2
+a += 3
+console.log(a);
+
+b -= 2
+console.log(b); 
+
+//Activity 3
+console.log(a > b);
+console.log(a < b);
+
+console.log(a >= b)
+console.log(a <= b)
+
+let str = "1"
+a = 1
+
+console.log(a == str);
+console.log(a === str);
+
+//Activity 4
+console.log(a > 5 && b < 4);
+console.log(a > 5 || b < 4);
+console.log(!false);
+
+//Activity 5
+a = -3
+console.log(a > 0 ? "Positive" : "Negative");
