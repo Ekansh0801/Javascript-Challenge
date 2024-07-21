95aceda10eff9372dab67d859f34aabb8f1e2c29
Ekansh0801 <ekanshaggarwal11103@gmail.com>
2024-07-21 12:55:58 +0530

    Day-04

--- Day-04/script.js
+++ Day-04/script.js
@@ -1,108 +1,58 @@
 // Activity - 1
+for(let i = 1; i <=10; i++){
+    console.log(i);
+} 
 
-let num = 5;
-if(num > 0){
-    console.log(num, "is Positive!");
-}
-else if(num < 0){
-    console.log(num, "is Negative!");
-}
-else{
-    console.log(num, "is Zero!");
+for(let i = 1; i <= 10; i++){
+    console.log(5 * i);
 }
 
-// Activity - 2
-let x = 2,y = 3,z = 4
-if(x > y){
-    if(x > z){
-        console.log(x, "is largest!!");
-    }
-    else{
-        console.log(z, "is largest!!");
-    }
+//Activity - 2
+let i = 1;
+let sum = 0;
+while(i <= 10){
+    sum += i;
+    i++;
+}
+console.log(sum);
+
+let num = 10;
+while(i >= 1){
+    console.log(i);
+    i--;
 }
 
-else{
-    if(y > z){
-        console.log(y, "is largest!!");
-    }
-    else{
-        console.log(z, "is largest!!");
-    }
-}
+//Activity - 3
+x = 1;
+do{
+    console.log(x);
+    x++;
+}while(x <= 5);
 
-
-// Activity - 3
-let day = 2
-switch(day){
-    case 1:{
-        console.log("Monday!");
-        break;
-    };
-    case 2: {
-        console.log("Tuesday!");
-        break;
-    };
-    case 3: {
-        console.log("Wednesday!");
-        break;
-    };
-    case 4: {
-        console.log("Thursday!");
-        break;
-    };
-    case 5: {
-        console.log("Friday!");
-        break;
-    };
-    case 6: {
-        console.log("Saturday!");
-        break;
-    };
-    case 7: {
-        console.log("Sunday!");
-        break;
-    };
-    default : {
-        console.log("Enter valid Number");
-    }
-}
-
-let marks = 70
-switch(true){
-    case marks >= 90 && marks <= 100:{
-        console.log("A!");
-        break;
-    };
-    case marks >= 80 && marks < 90: {
-        console.log("B!");
-        break;
-    };
-    case marks >= 70 && marks < 80: {
-        console.log("C!");
-        break;
-    };
-    case marks >= 60 && marks < 70: {
-        console.log("D!");
-        break;
-    };
-    case marks >= 50 && marks < 60: {
-        console.log("E!");
-        break;
-    };
-    default : {
-        console.log("F");
-    }
-}
-
-//Activity - 4
 let n = 5;
-console.log(n & 1 === 1 ? "odd!" : "even!");
+let fact = 1;
+do{
+    fact = fact * n;
+    n--;
+}while(n >= 1)
 
-//Activity - 5
-let year = 2016
-if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
-    console.log("Leap Year!!");
-} else {
-    console.log("Non Leap Year!!");
+    console.log(fact);
+
+    let a = 5;
+for(let i = 0; i < a; i++){
+    let row = "";
+    for(let j  = 0; j <= i; j++){
+        row += "*";
+    }
+    console.log(row);
+}    
+
+for(let i = 1; i <= 10; i++){
+    if(i == 5) continue;
+    console.log(i);
+}
+
+for(let i = 1; i <= 10; i++){
+    if(i == 7) break;
+    console.log(i);
 }
